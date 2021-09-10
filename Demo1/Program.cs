4776ba02f975441f12c5f9bdbb018ab6d16204dd
Oscar <oscargus0247@gmail.com>
2021-09-10 13:11:35 +0200

    Kommenterade koden

--- Demo1/Program.cs
+++ Demo1/Program.cs
@@ -7,9 +7,9 @@ namespace Demo1
     {
         static void Main(string[] args)
         {
-            List<Customer> myCustomers = new List<Customer>();
+            List<Customer> myCustomers = new List<Customer>(); // Skapa customerlistan
 
-            while (true)
+            while (true) // Programmet körs
             {
                 Console.WriteLine("Välj vad du vill göra med hjälp av alternativen nedan:\n" +
                                   "1 (Skapa ny kund)\n" +
@@ -27,9 +27,10 @@ namespace Demo1
                     Console.WriteLine("Mata in kundens nummer:");
                     string customerNumber = Console.ReadLine();
 
+                    // Lägg till kunden i listan
                     myCustomers.Add(new Customer() { Name = customerName, Adress = customerAdress, Number = customerNumber });
 
-                    Console.Clear();
+                    Console.Clear(); // Ta bort allting från consolen, blir väldigt rörigt annars
                     Console.WriteLine($"Ny kund \"{customerName}\" har skapats!");
                 }
 
@@ -44,6 +45,7 @@ namespace Demo1
                 {
                     Console.Clear();
 
+                    // Loopa genom listan och mata ut all information om varje Customer
                     foreach (Customer c in myCustomers)
                     {
                         Console.WriteLine($"Namn: {c.Name}\n" +
@@ -54,15 +56,17 @@ namespace Demo1
 
                 if (alternativ == "4") // Avsluta
                 {
-                    break;
+                    break; // Avsluta loopen
                 }
 
+                // Om input inte är ett av alternativen påpeka det så personen som matar in förstår vad som behövs göras
                 if (alternativ != "1" && alternativ != "2" && alternativ != "3" && alternativ != "4")
                 {
                     Console.Clear();
                     Console.WriteLine("Du måste skriva ett av alternativen 1, 2, 3 eller 4!");
                 }
 
+                // Gör så att man får lite rum att andas och läsa vad som händer innan alternativen postas igen
                 Console.ReadLine();
             }
         }
